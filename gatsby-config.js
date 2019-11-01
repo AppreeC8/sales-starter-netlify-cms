3d38783dfbec7967ac704803caa1c76732fa4fa5
David Davis <davidjamesdavis.djd@gmail.com>
2019-10-31 21:34:00 -0700

    mods

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-var proxy = require('./node_modules/http-proxy-middleware')
+var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
@@ -7,44 +7,44 @@ module.exports = {
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
   plugins: [
-    './node_modules/gatsby-plugin-react-helmet',
-    './node_modules/gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: './node_modules/gatsby-source-filesystem',
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/static/img`,
         name: 'uploads',
       },
     },
     {
-      resolve: './node_modules/gatsby-source-filesystem',
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
         name: 'pages',
       },
     },
     {
-      resolve: './node_modules/gatsby-source-filesystem',
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/img`,
         name: 'images',
       },
     },
-    './node_modules/gatsby-plugin-sharp',
-    './node_modules/gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: './node_modules/gatsby-transformer-remark',
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: './node_modules/gatsby-remark-relative-images',
+            resolve: 'gatsby-remark-relative-images',
             options: {
               name: 'uploads',
             },
           },
           {
-            resolve: './node_modules/gatsby-remark-images',
+            resolve: 'gatsby-remark-images',
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
@@ -53,7 +53,7 @@ module.exports = {
             },
           },
           {
-            resolve: './node_modules/gatsby-remark-copy-linked-files',
+            resolve: 'gatsby-remark-copy-linked-files',
             options: {
               destinationDir: 'static',
             },
@@ -62,19 +62,19 @@ module.exports = {
       },
     },
     {
-      resolve: './node_modules/gatsby-plugin-netlify-cms',
+      resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
     {
-      resolve: './node_modules/gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
+      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
-    './node_modules/gatsby-plugin-netlify', // make sure to keep it last in the array
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
   // for avoiding CORS while developing Netlify Functions locally
   // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
